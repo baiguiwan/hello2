7593b7910446886d049e409f1b3d4f4302085b9f
jujingyi <15999600272@163.com>
2019-08-09 20:46:37 +0800

    登陆

--- src/main/java/com/example/demo2/controller/AuthorizeController.java
+++ src/main/java/com/example/demo2/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -21,7 +23,7 @@ public class AuthorizeController {
     private String redirectUri;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code")String code,@RequestParam(name="state") String state){
+    public String callback(@RequestParam(name="code")String code, @RequestParam(name="state") String state, HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -30,7 +32,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String token=githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user=githubProvider.getUser(token);
-        System.out.println(user.getName());
-        return "index";
+        if(user!=null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+            //登陆成功
+        }else{
+            //登陆失败 重新登陆
+            return "redirect:index";
+        }
     }
 }
